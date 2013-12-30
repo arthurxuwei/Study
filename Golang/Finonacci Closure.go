d7b31d370d4e35fe3431f4c27d4eed1b89b584db
arthurxuwei <diandianx007@gmail.com>
2013-12-30 18:06:21 +0800

    Golang Start

---
+++ Golang/Finonacci Closure.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func fib() func() int {
+	a, b := 0,1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+}
+
+func main() {
+	f := fib()
+	fmt.Println(f(), f(), f(), f(), f())
+}
