d5d0e4b394544de0016ebe9dac0e997ed59401e7
arthurxuwei <diandianx007@gmail.com>
2014-05-06 18:44:11 +0800

    java version mutliThread

---
+++ Java/mutliThread/ProducerConsumer/Consumer.java
@@ -0,0 +1,43 @@
+package mutliThread.ProducerConsumer;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by weixu on 5/6/2014.
+ */
+public class Consumer implements Runnable {
+
+    public Consumer(BlockingQueue<String> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Start consumer thread.");
+        Random r = new Random();
+        boolean isRunning = true;
+        try {
+            while (isRunning) {
+                System.out.println("Receiving data from queue...");
+                String data = queue.poll(2, TimeUnit.SECONDS);
+                if (null != data) {
+                    System.out.println("Data：" + data);
+                    System.out.println("Consuming：" + data);
+                    Thread.sleep(r.nextInt(DEFAULT_RANGE_FOR_SLEEP));
+                } else {
+                    isRunning = false;
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        } finally {
+            System.out.println("Exit consumer thread.");
+        }
+    }
+
+    private BlockingQueue<String> queue;
+    private static final int DEFAULT_RANGE_FOR_SLEEP = 1000;
+}
---
+++ Java/mutliThread/ProducerConsumer/Producer.java
@@ -0,0 +1,51 @@
+package mutliThread.ProducerConsumer;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by weixu on 5/6/2014.
+ */
+public class Producer implements Runnable {
+
+    public Producer(BlockingQueue queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        String data;
+        Random r = new Random();
+
+        System.out.println("Start Producer Thread.");
+        try {
+            while (isRunning) {
+                System.out.println("Producing Data...");
+                Thread.sleep(r.nextInt(DEFAULT_RANGE_FOR_SLEEP));
+
+                data = "data: " + count.incrementAndGet();
+                System.out.println("Push "+ data + " into queue");
+                if (!queue.offer(data, 2, TimeUnit.SECONDS)) {
+                    System.out.println("Failure with: "+ data);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        } finally {
+            System.out.println("Exit producer thread.");
+        }
+    }
+
+    public void stop() {
+        isRunning = false;
+    }
+
+
+    private volatile boolean isRunning = true;
+    private BlockingQueue queue;
+    private static AtomicInteger count = new AtomicInteger();
+    private static final int DEFAULT_RANGE_FOR_SLEEP = 1000;
+}
