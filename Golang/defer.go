3cd5454249678e58988e1c937926a25472ddadb9
arthurxuwei <diandianx007@gmail.com>
2014-05-19 13:57:38 +0800

    go defer useage

---
+++ Golang/defer.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var whatever [5]struct{}
+
+	for i := range whatever {
+		fmt.Println(i)
+	}
+
+	for i := range whatever {
+		defer func() {fmt.Println(i)}()
+	}
+	
+	for i := range whatever {
+		defer func(n int) {fmt.Println(n)}(i)
+	}
+}
