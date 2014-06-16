02733d622eb9429c4954e03079deab3ecb5ba57f
arthurxuwei <diandianx007@gmail.com>
2014-06-16 17:18:59 +0800

    Module

--- FSharp/ObjectOriented/ModulesNamespaces.fsx
+++ FSharp/ObjectOriented/ModulesNamespaces.fsx
@@ -1,1 +1,18 @@
-﻿
+﻿//Defining Modules
+//moduleName.member
+//open keyword
+
+//submodules
+
+//module keyword
+
+//Extending Types and Modules
+//module moduleName = xxxxx
+
+//Module Signatures
+(*Signature files have the same name as their corresponding module, but end with a ".fsi" extension (f-sharp interface). 
+Signature files always come before their implementation files, which have a corresponding ".fs" extension*)
+
+//Define Namespace
+
+//namespace keyword
