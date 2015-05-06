d9e442c3e1ca04844bb3b85cbc8d4394a51c9e6a
arthur.xw <arthur.xw@alibaba-inc.com>
2015-05-06 20:59:03 +0800

    python Itertools

--- Python/Itertools.py
+++ Python/Itertools.py
@@ -1,13 +1,32 @@
 import itertools
 
-l = itertools.chain('ABC', 'DEFG')
+#first partition
+# count(start=0, step=1) -> 0,1,2,3....
+for i in itertools.izip(itertools.count(1), ['a', 'b', 'c']):
+	print i
 
-print list(l)
+# cycle('ABCD') -> A B C D A B C D...
+i=0
+for item in itertools.cycle(['a','b','c']):
+	i+=1
+	if i == 10:
+		break
+	print item
+	
+#repeat(10, 3) -> 10, 10, 10
+for i in repeat('again', 5):
+	print i
 
-l = itertools.product(range(2), repeat=3)
+#second partition
+#chain('ABC', 'DEF') -> A B C D E F
+for i in itertools.chain('ABC', 'DEFG'):
+	print i
+	
+#compress('ABCDEF', [1,0,1,0,1,1]) --> A C E F
+for i in itertools.compress('ABCDEF', [1,0,1,0,1,1]):
+	print i
 
-print list(l)
+itertools.product(range(2), repeat=3)
 
-l = itertools.permutations(range(3))
+itertools.permutations(range(3))
 
-print list(l)
