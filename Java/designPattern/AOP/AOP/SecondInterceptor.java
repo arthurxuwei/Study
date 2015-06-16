334b1d50931e6476cb5bccd90e1f8416b37d495c
arthur.xw <arthur.xw@alibaba-inc.com>
2015-06-16 21:04:54 +0800

    proxy & AOP

---
+++ Java/designPattern/AOP/AOP/SecondInterceptor.java
@@ -0,0 +1,14 @@
+package Proxy;
+
+/**
+ * Created by arthur.xw on 2015/6/16.
+ */
+public class SecondInterceptor implements Interceptor {
+
+    @Override
+    public void intercept(ActionInvocation invocation) {
+        System.out.println(2);
+        invocation.invoke();
+        System.out.println(-2);
+    }
+}
---
+++ Java/designPattern/Proxy/dynamicProxy/BookFacade.java
@@ -0,0 +1,8 @@
+package Proxy;
+
+/**
+ * Created by arthur.xw on 2015/6/16.
+ */
+public interface BookFacade {
+    public void addBook();
+}
