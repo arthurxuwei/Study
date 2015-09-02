5cac28abdbe251ff1d80bf076d4341c3800f5c37
arthur.xw <arthur.xw@alibaba-inc.com>
2015-09-02 20:50:13 +0800

    etcd_client

--- iaas/etcd_client.go
+++ iaas/etcd_client.go
@@ -1,14 +1,24 @@
 package main
-import(
+
+import (
 	"fmt"
 	"github.com/coreos/go-etcd/etcd"
 )
 
 func main() {
-	machines := []string{"http://172.17.8.101:2375"}
-	client := etch.NewClient(machines)
-	
-	if _, err :=client.Set("/foo", "bar", 0); err != null{
-		fmt.println(err)
+	machines := []string{"http://172.17.8.101:4001"}
+	client := etcd.NewClient(machines)
+
+	if _, err := client.Set("/foo", "bar", 0); err != nil {
+		fmt.Println(err)
 	}
+	fmt.Println("done")
+	value, err := client.Get("/foo", false, false)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("key: " + value.Node.Key)
+		fmt.Println("value: " + value.Node.Value)
+	}
+
 }
