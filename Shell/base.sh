a93d79a1c6f990579fece3d01efe69f57c0465d5
伏顼 <arthur.xw@alibaba-inc.com>
2015-11-12 21:09:15 +0800

    shell

--- Shell/base.sh
+++ Shell/base.sh
@@ -35,18 +35,20 @@ lsscsi
 #all
 sosreport
 
-//log
-2013-09-08 21:00:54 SMTP connection from [78.110.75.245]:5387 (TCP/IP connection count = 20)
-2013-09-08 21:00:54 SMTP connection from [188.175.142.13]:34332 (TCP/IP connection count = 20)
-2013-09-08 21:45:41 SMTP connection from [58.137.11.145]:51984 (TCP/IP connection count = 20)
-2013-09-08 21:49:26 SMTP connection from [109.93.248.151]:22273 (TCP/IP connection count = 20)
-2013-09-08 21:49:27 SMTP connection from [37.131.64.203]:7906 (TCP/IP connection count = 20)
-
-//shell
-awk -F'[][]' '{print $2}' ip_practices.txt 
-
 //example 2
 grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' ip_practices.txt
 
+//
+chmod chown chgrp --reference
+
+//more ls
+stat {file}
+
+//file type
+file {file}
+
+
+//
+diff cmp comm md5sum
 
  
