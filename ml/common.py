a2ea19736ddeb64dde918c9894f43a2af56e05bc
arthur.xw <arthur.xw@alibaba-inc.com>
2019-03-07 19:04:24 +0800

    neural network

---
+++ ml/common.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def tanh(x):
+    return np.tanh(x)
+
+
+def tanh_deriv(x):
+    return 1.0 - np.tanh(x)**2
+
+
+def logistic(x):
+    return 1/(1 + np.exp(-x))
+
+
+def logistic_derivative(x):
+    return logistic(x)*(1-logistic(x))
