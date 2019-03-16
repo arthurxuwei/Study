1b741baa9f0473345b1ae578718412e7a8ec82bf
arthur.xw <arthur.xw@alibaba-inc.com>
2019-03-16 16:48:48 +0800

    add regression

--- ml/supervised/regression.py
+++ ml/supervised/regression.py
@@ -1,5 +1,9 @@
 import math
+from typing import Callable
+
 import numpy as np
+
+from utils.utils import normalize, polynomial_features
 
 
 class Regularization:
@@ -10,42 +14,92 @@ class Regularization:
         self.alpha = alpha
 
     def __call__(self, w):
+        return self.alpha * w
+
+    def grad(self, w):
+        return self.alpha * w
+
+
+class L1Regularization(Regularization):
+    """
+    L1 Regularization
+    """
+    def __init__(self, alpha):
+        super(L1Regularization, self).__init__(alpha=alpha)
+
+    def __call__(self, w):
         return self.alpha * np.linalg.norm(w)
 
     def grad(self, w):
         return self.alpha * np.sign(w)
 
 
+class L2Regularization(Regularization):
+    """
+    L2 Regularization
+    """
+    def __init__(self, alpha):
+        super(L2Regularization, self).__init__(alpha=alpha)
+
+    def __call__(self, w):
+        return self.alpha * 0.5 * w.T.dot(w)
+
+    def grad(self, w):
+        return self.alpha * w
+
+
+class L1L2Regularization(Regularization):
+    """
+    L1L2 Regularization
+    """
+    def __init__(self, alpha, l1_ratio=0.5):
+        super(L1L2Regularization, self).__init__(alpha=alpha)
+        self.l1_ratio = l1_ratio
+
+    def __call__(self, w):
+        l1_contr = self.l1_ratio * np.linalg.norm(w)
+        l2_contr = (1 - self.l1_ratio) * 0.5 * w.T.dot(w)
+        return self.alpha * (l1_contr + l2_contr)
+
+    def grad(self, w):
+        l1_contr = self.l1_ratio * np.sign(w)
+        l2_contr = (1 - self.l1_ratio) * w
+        return self.alpha * (l1_contr + l2_contr)
+
+
 class Regression(object):
     """
-        Base regression model.
+        Regression interface
     """
     def __init__(self, n_iterations: int, learning_rate: float):
-        """
-        :param n_iterations: training iterations
-        :param learning_rate: the step length that will be used when updating the weights
-        """
-        self.training_errors = []
         self.n_iterations = n_iterations
         self.learning_rate = learning_rate
-        self.w = np.array((0, 0))
-        # No regularization
-        self.regularization = Regularization(0)
 
-    def initialize_weights(self, n_features: int):
-        """
-        Init weights
-        :param n_features: number of feature
-        """
-        limit = 1 / math.sqrt(n_features)
-        self.w = np.random.uniform(-limit, limit, (n_features,))
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+
+class GDLinearRegression(Regression):
+    """
+        Linear model with gradient descent.
+    """
+    def __init__(self, n_iterations: int, learning_rate: float, regularization: Regularization = Regularization(0)):
+        super(GDLinearRegression, self).__init__(n_iterations, learning_rate)
+        self.regularization = regularization
+        self.training_errors = []
+        self.w = np.array((0, 0))
 
     def fit(self, X, y):
         # insert constant ones for bias weights
         X = np.insert(X, 0, 1, axis=1)
-        self.initialize_weights(X.shape[1])
+        # init weights
+        limit = 1 / math.sqrt(X.shape[1])
+        self.w = np.random.uniform(-limit, limit, (X.shape[1],))
 
-        # Do gradient descent for n_iterations
+        # Do gradient descent
         for i in range(self.n_iterations):
             y_pred = X.dot(self.w)
 
@@ -58,3 +112,113 @@ class Regression(object):
     def predict(self, X):
         X = np.insert(X, 0, 1, axis=1)
         return X.dot(self.w)
+
+
+class SVDLinearRegression(Regression):
+    """
+        Linear model with SVD.
+    """
+    def __init__(self, n_iterations: int = 100,  learning_rate: float = 0.001):
+        super(SVDLinearRegression, self).__init__(n_iterations, learning_rate)
+        self.w = np.array(0, 0)
+
+    def fit(self, X, y):
+        # insert constant ones for bias weights
+        X = np.insert(X, 0, 1, axis=1)
+        # Do SVD
+        U, S, V = np.linalg.svd(X.T.dot(X))
+        S = np.diag(S)
+        X_sq_reg_inv = V.dot(np.linalg.pinv(S)).dot(U.T)
+        self.w = X_sq_reg_inv.dot(X.T).dot(y)
+
+    def predict(self, X):
+        X = np.insert(X, 0, 1, axis=1)
+        return X.dot(self.w)
+
+
+class PolynomialRegression(GDLinearRegression):
+    """
+        Performs a non-linear transformation of the data before fitting and predictions
+        Allows for non-linear regression
+    """
+    def __init__(self, degree, n_iterations=3000, learning_rate=0.01):
+        """
+        :param degree: the degree of the polynomial
+        """
+        super(PolynomialRegression, self).__init__(n_iterations, learning_rate)
+        self.degree = degree
+
+    def fit(self, X, y):
+        X = polynomial_features(X, degree=self.degree)
+        super(PolynomialRegression, self).fit(X, y)
+
+    def predict(self, X):
+        X = polynomial_features(X, degree=self.degree)
+        return super(PolynomialRegression, self).predict(X)
+
+
+class RidgeRegression(GDLinearRegression):
+    """
+    Linear regression model with a regularization factor.Model that
+    tries to balance the fit of the model with respect to the training data and the complexity of the model.
+    A large regularization factor with decreases the variance of the model and do para
+    """
+    def __init__(self, reg_factor, n_iterations=1000, learning_rate=0.001):
+        """
+        :param reg_factor: the factor that determine the amount of regularization and feature
+        """
+        super(RidgeRegression, self).__init__(n_iterations, learning_rate, L2Regularization(reg_factor))
+
+
+class LassoRegression(GDLinearRegression):
+    """
+        Linear model with a regularization factor which does both variable selection and regularization.
+    """
+    def __init__(self, degree, reg_factor, n_iterations=3000, learning_rate=0.01):
+        super(LassoRegression, self).__init__(n_iterations, learning_rate, L1Regularization(reg_factor))
+        self.degree = degree
+
+    def fit(self, X, y):
+        X = normalize(polynomial_features(X, degree=self.degree))
+        super(LassoRegression, self).fit(X, y)
+
+    def predict(self, X):
+        X = normalize(polynomial_features(X, degree=self.degree))
+        return super(LassoRegression, self).predict(X)
+
+
+class PolynomialRidgeRegression(GDLinearRegression):
+    """
+    Similar to regular ridge regression except that the data is transformed to allow for polynomial regression.
+    """
+    def __init__(self, degree, reg_factor, n_iterations=3000, learning_rate=0.01):
+        super(PolynomialRidgeRegression, self).__init__(n_iterations, learning_rate, L2Regularization(reg_factor))
+        self.degree = degree
+
+    def fit(self, X, y):
+        X = normalize(polynomial_features(X, degree=self.degree))
+        super(PolynomialRidgeRegression, self).fit(X, y)
+
+    def predict(self, X):
+        X = normalize(polynomial_features(X, degree=self.degree))
+        super(PolynomialRidgeRegression, self).predict(X)
+
+
+class ElasticNet(GDLinearRegression):
+    """
+    Regression where a combination of L1 and L2 regularization are used
+    """
+    def __init__(self, degree=1, reg_factor=0.05, l1_ratio=0.5, n_iterations=3000, learning_rate=0.01):
+        """
+        :param l1_ratio: Weighs the contribution of l1 and l2 regularization.
+        """
+        super(ElasticNet, self).__init__(n_iterations, learning_rate, L1L2Regularization(reg_factor, l1_ratio=l1_ratio))
+        self.degree = degree
+
+    def fit(self, X, y):
+        X = normalize(polynomial_features(X, degree=self.degree))
+        super(ElasticNet, self).fit(X, y)
+
+    def predict(self, X):
+        X = normalize(polynomial_features(X, degree=self.degree))
+        super(ElasticNet, self).predict(X)
---
+++ ml/utils/utils.py
@@ -0,0 +1,39 @@
+import itertools
+
+import numpy as np
+
+
+def normalize(X, axis=-1, order=2):
+    """
+    Normalize the data
+    :param X: dataset
+    :param axis: @see numpy.linalg.norm
+    :param order: @see numpy.linalg.norm
+    :return: normalized dataset
+    """
+    l2 = np.atleast_1d(np.linalg.norm(X, order, axis))
+    l2[l2 == 0] = 1
+    return X / np.expand_dims(l2, axis)
+
+
+def polynomial_features(X, degree):
+    """
+    :param X: dataset
+    :param degree: number of features
+    :return: new dataset
+    """
+    n_samples, n_features = np.shape(X)
+
+    def index_combinations():
+        combs = [itertools.combinations_with_replacement(range(n_features), i) for i in range(0, degree + 1)]
+        flat_combs = [i for sublist in combs for i in sublist]
+        return flat_combs
+
+    combinations = index_combinations()
+    n_output_features = len(combinations)
+    X_new = np.empty((n_samples, n_output_features))
+
+    for index, index_combs in enumerate(combinations):
+        X_new[:, index] = np.prod(X[:, index_combs], axis=1)
+
+    return X_new
