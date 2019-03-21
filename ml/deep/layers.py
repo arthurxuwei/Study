fc60b224c67bc1c5528c20bfd74c2371543dd4b1
arthur.xw <arthur.xw@alibaba-inc.com>
2019-03-21 09:41:13 +0800

    add layers: Conv2D

--- ml/deep/layers.py
+++ ml/deep/layers.py
@@ -155,7 +155,7 @@ class RNN(Layer):
         # Back Propagation Through Time
         for t in reversed(range(timesteps)):
             # Update gradient w.r.t V at time step t
-            grad_v += accum_grad[:, t].T.dot(self.states[:, t])
+            grad_V += accum_grad[:, t].T.dot(self.states[:, t])
             # Calculate the gradient w.r.t the state input
             grad_wrt_state = accum_grad[:, t].dot(self.V) * self.activation.gradient(self.state_input[:, t])
             # Gradient w.r.t the layer input
@@ -177,3 +177,138 @@ class RNN(Layer):
 
     def output_shape(self):
         return self.input_shape
+
+
+class Conv2D(Layer):
+    """
+    2D Convolution Layer
+    """
+    def __init__(self, n_filters, filter_shape, input_shape=None, padding='same', stride=1):
+        super(Conv2D, self).__init__()
+        self.n_filters = n_filters
+        self.filter_shape = filter_shape
+        self.padding = padding
+        self.stride = stride
+        self.input_shape = input_shape
+        self.trainable = True
+
+    def initialize(self, optimizer):
+        filter_height, filter_width = self.filter_shape
+        channels = self.input_shape[0]
+        limit = 1 / math.sqrt(np.prod(self.filter_shape))
+        self.w = np.random.uniform(-limit, limit, size=(self.n_filters, channels, filter_height, filter_width))
+        self.w0 = np.zeros((self.n_filters, 1))
+        self.w_opt = copy.copy(optimizer)
+        self.w0_opt = copy.copy(optimizer)
+
+    def parameters(self):
+        return np.prod(self.w.shape) + np.prod(self.w0.shape)
+
+    def forward_pass(self, X, training=True):
+        batch_size,  channels, height, width = X.shape
+        self.layer_input = X
+        # Turn image shape into column shape (enables dot product between input and weights)
+        self.X_col = image_to_column(X, self.filter_shape, stride=self.stride, output_shape=self.padding)
+        self.w_col = self.w.reshape((self.n_filters, -1))
+        output = self.w_col.dot(self.X_col) + self.w0
+        output = output.reshape(self.output_shape() + (batch_size,))
+        return output.transpose(3, 0, 1, 2)
+
+    def backward_pass(self, accum_grad):
+        accum_grad = accum_grad.transpose(1, 2, 3, 0).reshape(self.n_filters, -1)
+        if self.trainable:
+            # Take dot product between column shaped accum. gradient and column shape
+            # layer input to determine the gradient at the layer with respect to layer weights
+            grad_w = accum_grad.dot(self.X_col.T).reshape(self.w.shape)
+            # The gradient with respect to bias terms is the sum similarly to in Dense layer
+            grad_w0 = np.sum(accum_grad, axis=1, keepdims=True)
+
+            self.w = self.w_opt.update(self.w, grad_w)
+            self.w0 = self.w0_opt.update(self.w0, grad_w0)
+
+        # Recalculate the gradient which will be propogated back to prev. layer
+        accum_grad = self.w_col.T.dot(accum_grad)
+        # Reshape from column shape to image shape
+        accum_grad = column_to_image(accum_grad, self.layer_input.shape, self.filter_shape, self.stride, self.padding)
+        return accum_grad
+
+    def output_shape(self):
+        channels, height, width = self.input_shape
+        pad_h, pad_w = determine_padding(self.filter_shape, output_shape=self.padding)
+        output_height = (height + np.sum(pad_h) - self.filter_shape[0]) / self.stride + 1
+        output_width = (width + np.sum(pad_w) - self.filter_shape[1]) / self.stride + 1
+        return self.n_filters, int(output_height), int(output_width)
+
+
+def determine_padding(filter_shape, output_shape="same"):
+    """
+    Method which calculates the padding based on the specified output shape and the shape of the filters
+    """
+    if output_shape == "valid":
+        # no padding
+        return (0, 0), (0, 0)
+    elif output_shape == "same":
+        # same shape between input and output
+        filter_height, filter_width = filter_shape
+        pad_h1 = int(math.floor((filter_height - 1)/2))
+        pad_h2 = int(math.ceil((filter_height - 1)/2))
+        pad_w1 = int(math.floor((filter_width - 1)/2))
+        pad_w2 = int(math.ceil((filter_width - 1)/2))
+        return (pad_h1, pad_h2), (pad_w1, pad_w2)
+
+
+def image_to_column(images, filter_shape, stride, output_shape='same'):
+    """
+    Method which turns the image shaped input to column shape.
+    """
+    filter_height, filter_width = filter_shape
+    pad_h, pad_w = determine_padding(filter_shape, output_shape)
+    # Add padding
+    images_padded = np.pad(images, ((0, 0), (0, 0), pad_h, pad_w), mode='constant')
+    # Calculate the indices where the dot products are to be applied between weights and the image
+    k, i, j = get_im2col_indices(images.shape, filter_shape, (pad_h, pad_w), stride)
+
+    # Get content from image at those indices
+    cols = images_padded[:, k, i, j]
+    channels = images.shape[1]
+
+    cols = cols.transpose(1, 2, 0).reshape(filter_height * filter_width * channels, -1)
+    return cols
+
+
+def column_to_image(cols, image_shapes, filter_shape, stride, output_shape='same'):
+    """
+    Method which turns the column shaped input to image shape
+    """
+    batch_size, channels, height, width = image_shapes
+    pad_h, pad_w = determine_padding(filter_shape, output_shape)
+    height_padded = height + np.sum(pad_h)
+    width_padded = width + np.sum(pad_w)
+    image_padded = np.empty((batch_size, channels, height_padded, width_padded))
+    k, i, j = get_im2col_indices(image_shapes, filter_shape, (pad_h, pad_w), stride)
+
+    cols = cols.reshape(channels * np.prod(filter_shape), -1, batch_size)
+    cols = cols.transpose(2, 0, 1)
+    # add column content to the images at the indices
+    np.add.at(image_padded, (slice(None), k, i, j), cols)
+
+    return image_padded[:, :, pad_h[0]:height + pad_h[0], pad_w[0]:width + pad_w[0]]
+
+
+def get_im2col_indices(imags_shape, filter_shape, padding, stride=1):
+    # First figure out what the size of the output should be
+    batch_size, channels, height, width = imags_shape
+    filter_height, filter_width = filter_shape
+    pad_h, pad_w = padding
+    out_height = int((height + np.sum(pad_h) - filter_height) / stride + 1)
+    out_width = int((width + np.sum(pad_w) - filter_width) / stride + 1)
+    i0 = np.repeat(np.arange(filter_height), filter_width)
+    i0 = np.tile(i0, channels)
+    i1 = stride * np.repeat(np.arange(out_height), out_width)
+    j0 = np.tile(np.arange(filter_width), filter_height * channels)
+    j1 = stride * np.tile(np.arange(out_width), out_height)
+    i = i0.reshape(-1, 1) + i1.reshape(1, -1)
+    j = j0.reshape(-1, 1) + j1.reshape(1, -1)
+    k = np.repeat(np.arange(channels), filter_height * filter_width).reshape(-1, 1)
+    return k, i, j
+
