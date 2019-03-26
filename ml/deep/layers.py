fdbc02cc9e85142bfec239fca90464de57535d76
arthur.xw <arthur.xw@alibaba-inc.com>
2019-03-26 13:46:11 +0800

    flatten

--- ml/deep/layers.py
+++ ml/deep/layers.py
@@ -483,6 +483,25 @@ class ZeroPadding2D(ConstantPadding2D):
         self.padding = 0
 
 
+class Flatten(Layer):
+    """
+    Turns a multidimensional matrix into two-dimensional
+    """
+    def __init__(self, input_shape=None):
+        self.prev_shape = None
+        self.trainable = True
+        self.input_shape = input_shape
+
+    def forward_pass(self, X, training=True):
+        self.prev_shape = X.shape
+        return X.reshape((X.shape[0], -1))
+
+    def backward_pass(self, accum_grad):
+        return accum_grad.reshape(self.prev_shape)
+
+    def output_shape(self):
+        return (np.prod(self.input_shape), )
+
 
 
 
