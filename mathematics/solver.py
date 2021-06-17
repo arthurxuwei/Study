65b6066b3494a83ac099f936184e2c67a91026f1
arthur.xw <arthur.xw@alibaba-inc.com>
2021-06-17 15:42:42 +0800

    sympy

--- mathematics/solver.py
+++ mathematics/solver.py
@@ -1,8 +1,13 @@
 from sympy import *
+init_printing(use_unicode=True)
 x = Symbol('x', real=True)
+
+print(diff(sin(x)*exp(x), x))
+t = Symbol('t', real=True)
 print(solve(Eq(x**2 - 1, 0), x))
 print(solve(x**2 > 4))
 
 print(expand((x + 1)**2))
 
-
+y = Function('y')
+print(dsolve(Eq(y(t).diff(t, t) - y(t), exp(t)), y(t)))
