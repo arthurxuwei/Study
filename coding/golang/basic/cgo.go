0c88c1d07c60fe0517730804fbff9c22ec6c3b42
arthur.xw <arthur.xw@alibaba-inc.com>
2022-09-16 14:49:07 +0800

    org

--- coding/golang/basic/cgo.go
+++ coding/golang/basic/cgo.go
@@ -1,13 +1,13 @@
 package main
+
 /*
 #include <unistd.h>
 */
 import "C"
 
 import (
-    "fmt"
+	"fmt"
 )
-
 
 func main() {
 	i, err := C.sysconf(C._SC_NPROCESSORS_ONLN)
@@ -16,7 +16,7 @@ func main() {
 	// error in returned value not in errno.
 	// (https://sourceware.org/bugzilla/show_bug.cgi?id=21536)
 	if i == -1 {
-        fmt.Println("err: %s", err)
+		fmt.Printf("err: %v\n", err)
 	}
-    fmt.Printf("Online CPU num: %d\n", i)
+	fmt.Printf("Online CPU num: %d\n", i)
 }
